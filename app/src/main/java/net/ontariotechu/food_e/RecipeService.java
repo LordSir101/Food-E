f85acd1796b341182c0718230890f8b8a92c0773
LordSir101 <tywunderlich15@gmail.com>
2022-11-14 22:18:31 -0500

    use settings when searching recipes

--- app/src/main/java/net/ontariotechu/food_e/RecipeService.java
+++ app/src/main/java/net/ontariotechu/food_e/RecipeService.java
@@ -1,8 +1,12 @@
 package net.ontariotechu.food_e;
 
+import android.app.Application;
+import android.content.Context;
 import android.text.TextUtils;
 
 import androidx.annotation.Nullable;
+
+import net.ontariotechu.food_e.ui.MainActivity;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -13,18 +17,30 @@ import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-public class RecipeService {
+public class RecipeService extends Application {
 
     private static RecipeService instance;
+    private static Context context;
     private final String baseUrl;
 
-    private RecipeService() {
+    private DbHandler db;
+    private List<String> allergies;
+    private List<String> diets;
+
+    public RecipeService() {
         this.baseUrl = String.format("https://api.edamam.com/api/recipes/v2?type=public&app_id=%s&app_key=%s", BuildConfig.APP_ID, BuildConfig.API_KEY);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        RecipeService.context = getApplicationContext();
     }
 
     // Thread safe instantiation
@@ -51,6 +67,22 @@ public class RecipeService {
     private List<Recipe> getRecipes(@Nullable String search, @Nullable Hashtable<String, ArrayList<String>> filters) {
 
         StringBuilder url = new StringBuilder(baseUrl);
+
+        db = new DbHandler(context);
+        allergies = Arrays.asList(db.getAllergies());
+        diets = Arrays.asList(db.getDiets());
+        db.close();
+
+        if(!allergies.isEmpty()){
+            for(String preference:allergies){
+                url.append("&health=").append(preference);
+            }
+        }
+        if(!diets.isEmpty()){
+            for(String preference:diets){
+                url.append("&diet=").append(preference);
+            }
+        }
 
         if (TextUtils.isEmpty(search) && (filters == null || filters.size() == 0))
             url.append("&mealType=breakfast&mealType=Lunch&mealType=Dinner");
